3756593ff002f1350ed7d9fcbea381d1237203d2
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2023-03-22 21:52:33 +0800

    finish 3 reg

--- strategies/select_module.py
+++ strategies/select_module.py
@@ -11,6 +11,7 @@ from avalanche.training.plugins.evaluation import default_evaluator
 from avalanche.training.templates import SupervisedTemplate
 
 from models.module_net import ModuleNet
+from strategies.hsic import hsic_normalized
 
 
 class SparseSelectionPlugin(SupervisedPlugin):
@@ -25,21 +26,28 @@ class SparseSelectionPlugin(SupervisedPlugin):
         super().__init__()
         self.alpha = alpha
         self.activate = activate
+        self.structure_loss = 0
+
+    def after_forward(self, strategy, **kwargs):
+        pass
 
     def before_backward(self, strategy, **kwargs):
         """
         Add mean loss
         """
         if self.activate:
-            selected_idxs_each_layer = strategy.model.selected_idxs_each_layer
+            selected_idxs_each_layer = strategy.model.backbone.selected_idxs_each_layer
             # [n_layers* tensor[bs, n_modules]]: [4* tensor[64, 8]]
 
             structure_loss = torch.mean(torch.stack(selected_idxs_each_layer))
             strategy.loss += self.alpha * structure_loss
 
+            self.structure_loss = structure_loss.item()
+
 
 class IndependentSelectionPlugin(SupervisedPlugin):
     """ The selection of modules in each layer should be independent for different labels.
+    Note that independent does not mean different.
     """
 
     def __init__(self, alpha=1., activate=True):
@@ -50,13 +58,43 @@ class IndependentSelectionPlugin(SupervisedPlugin):
         super().__init__()
         self.alpha = alpha
         self.activate = activate
+        self.structure_loss = 0
 
     def before_backward(self, strategy, **kwargs):
         """
         Add HSIC loss
         """
-        # if self.activate:
-        pass
+        if self.activate:
+            selected_idxs_each_layer = strategy.model.backbone.selected_idxs_each_layer
+            # [n_layers* tensor[bs, n_modules]]: [4* tensor[64, 8]]
+            selected_idxs_each_layer = torch.stack(selected_idxs_each_layer, dim=1)     # [64, 4, 8]
+            bs, n_layers, n_modules = selected_idxs_each_layer.shape
+            selected_idxs = selected_idxs_each_layer.reshape(bs, -1)
+            dim = n_layers * n_modules
+            # [bs, dim]: [64, 32]
+
+            '''labels for this batch'''
+            structure_loss = 0
+            batch_labels = strategy.mbatch[1]       # [bs,]: [64]
+            label_set = sorted(set(batch_labels.tolist()))
+            if len(label_set) == 1:
+                # only one class in this mini batch, do not need to cal independent loss
+                return
+
+            prototypes = []     # [n_class, 32]
+            for label in label_set:
+                prototype = torch.mean(selected_idxs[batch_labels == label], dim=0)    # [32]
+                prototypes.append(prototype)
+
+            prototypes = torch.stack(prototypes)
+            pair_comb_protos = prototypes[torch.combinations(torch.arange(prototypes.shape[0]))]
+            # [n_comb, 2, 32]
+            A, B = pair_comb_protos[:, 0, :], pair_comb_protos[:, 1, :]
+
+            structure_loss = hsic_normalized(A, B)
+            strategy.loss += self.alpha * structure_loss
+
+            self.structure_loss = structure_loss.item()
 
 
 class ConsistentSelectionPlugin(SupervisedPlugin):
@@ -71,19 +109,38 @@ class ConsistentSelectionPlugin(SupervisedPlugin):
         super().__init__()
         self.alpha = alpha
         self.activate = activate
+        self.structure_loss = 0
 
     def before_backward(self, strategy, **kwargs):
         """
-        Add distance loss
+        Add distance loss, dot product
         """
         if self.activate:
-            selected_idxs_each_layer = strategy.model.selected_idxs_each_layer
+            selected_idxs_each_layer = strategy.model.backbone.selected_idxs_each_layer
             # [n_layers* tensor[bs, n_modules]]: [4* tensor[64, 8]]
+            selected_idxs_each_layer = torch.stack(selected_idxs_each_layer, dim=1)     # [64, 4, 8]
+            bs, n_layers, n_modules = selected_idxs_each_layer.shape
+            selected_idxs = selected_idxs_each_layer.reshape(bs, -1)
+            dim = n_layers * n_modules
+            # [bs, dim]: [64, 32]
 
             '''labels for this batch'''
-            batch_labels = strategy.mbatch[1]
+            structure_loss = []
+            batch_labels = strategy.mbatch[1]       # [bs,]: [64]
+            label_set = sorted(set(batch_labels.tolist()))
+            for label in label_set:
+                A = selected_idxs[batch_labels == label]
+                sim_matrix = A @ A.T
+                # mask diag     since increase diag elements (self dot prod) means encourage all 1 (all select).
+                sim_matrix = sim_matrix - torch.diag_embed(torch.diag(sim_matrix))
 
-            # structure_loss =
+                structure_loss.append(torch.mean(sim_matrix))
+
+            structure_loss = torch.sum(torch.stack(structure_loss))
+            structure_loss = - structure_loss       # minimize negative similarity.
+            strategy.loss += self.alpha * structure_loss
+
+            self.structure_loss = structure_loss.item()
 
 
 class Algorithm(SupervisedTemplate):
